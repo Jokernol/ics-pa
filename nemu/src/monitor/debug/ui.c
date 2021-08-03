27fa3fdcd6e7443a6e87896a3731bec29409cfae
tracer-ics2020 <tracer@njuics.org>
2021-08-03 15:49:23 +0800

    >  compile 191220000  Static hostname: zhu        Icon name: computer-laptop          Chassis: laptop       Machine ID: 225871ccac5d46e3ad33f9895860c642          Boot ID: e22e7b7e6e0e4808a6cb378f71f4ed6a Operating System: Arch Linux           Kernel: Linux 5.13.6-arch1-1     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Yoga 14sARH 2021  15:49:23 up 1 day,  5:22,  2 users,  load average: 1.52, 1.23, 1.32

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -86,10 +86,10 @@ static int cmd_help(char *args) {
 }
 
 static int cmd_x(char *args) {
-  printf("%s", args);
-  //char *arg1 = strtok(NULL, " ");
+  char *arg1 = strtok(NULL, " ");
   //char *arg2 = __strtok_r(NULL, " ");
-
+  printf("%s\n", args);
+  printf("%s\n", arg1);
   //paddr_t addr;
   //bool success;
 
