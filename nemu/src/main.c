874edeaa7ea6e490ce33eec98b48fb3b4b692d90
tracer-ics2020 <tracer@njuics.org>
2021-08-07 13:56:34 +0800

    >  compile 191220000

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,13 +1,29 @@
+#include <stdio.h>
+
 void init_monitor(int, char *[]);
 void engine_start();
 int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {
+  FILE* fp = fopen("input.txt", "r");
+  char str[256] = {'\0'};
+
+  if(fp == NULL) {
+    perror("打开文件时发生错误");
+    return -1;
+  }
+
+  while(fgets(str, 256, fp) != NULL) {
+    puts(str);
+  }
+
+  fclose(fp);
   /* Initialize the monitor. */
-  init_monitor(argc, argv);
-
+  //init_monitor(argc, argv);
+  
   /* Start engine. */
-  engine_start();
+  //engine_start();
 
-  return is_exit_status_bad();
+  return 0;
+  //return is_exit_status_bad();
 }
