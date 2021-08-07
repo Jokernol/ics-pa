dbdbc242cb63e8203390225d28f61f20c9693d88
tracer-ics2020 <tracer@njuics.org>
2021-08-07 20:05:27 +0800

    >  compile 191220000

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,49 +1,7 @@
-#include <stdio.h>
-#include <isa.h>
-#include <stdlib.h>
-
 void init_monitor(int, char *[]);
 void engine_start();
 int is_exit_status_bad();
-/*
-word_t expr(char *e, bool *success);
 
-static int file_gets() {
-  FILE* fp = fopen("/home/jokernol/Anything/ics-pa/nemu/tools/gen-expr/input", "r");
-  char str[256] = {};
-
-  if(fp == NULL) {
-    perror("");
-    return -1;
-  }
-  
-  while(fgets(str, 256, fp) != NULL) {
-    char *ans = strtok(str, " ");
-    char *exp = ans + strlen(ans) + 1;
-    
-    exp[strlen(exp) - 1] = '\0';
-
-    printf("%ld %s %s\n", strlen(exp), ans, exp);
-
-    
-    bool success = true;
-    word_t res = expr(exp, &success);
-//
-    printf("%d", res);
-    //if (expr(exp, &success) == atoi(ans)) {
-    //  printf("%s %s %s\n", ans, exp, "True");
-    //} else {
-    //  printf("%s %s %s\n", ans, exp, "False");
-    //}
-
-    //memset(str, '\0', sizeof(str));
-  }
-
-  fclose(fp);
-
-  return 0;
-}
-*/
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   init_monitor(argc, argv);
@@ -52,9 +10,6 @@ int main(int argc, char *argv[]) {
   engine_start();
 
   return is_exit_status_bad();
-  //file_gets();
-//
-  //return 0;
 }
 
 
