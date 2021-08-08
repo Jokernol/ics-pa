a5248d8d2c949b7dba00590d6591cf77e4f3ff8a
tracer-ics2020 <tracer@njuics.org>
2021-08-08 13:03:35 +0800

    >  compile 191220000

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -27,31 +27,53 @@ static uint8_t choose(uint8_t n) {
 
 static void gen_num() {
   char str[16];
+  char hex_str[16];
   
-  sprintf(str, "%u", rand() % 100);
+  uint16_t num = rand() % 100;
+
+  sprintf(str, "%u", num);
+  sprintf(hex_str, "0x%X", num);
 
   uint32_t len = strlen(str);
-  
+  uint32_t hex_len = strlen(hex_str);
+
   strncpy(p, str, len);
+  strncpy(q, hex_str, hex_len);
 
   p += len;
+  q += hex_len;
 }
 
 static void gen(char c) {
   *p = c;
+  *q = c;
 
   p ++;
+  q ++;
 }
 
 static void gen_rand_op() {
   switch (choose(4)) {
-    case 0: *p = '+'; break;
-    case 1: *p = '-'; break;
-    case 2: *p = '*'; break;
-    case 3: *p = '/'; break;
+    case 0: 
+      *p = '+';
+      *q = '+';
+      break;
+    case 1: 
+      *p = '-';
+      *q = '-';
+      break;
+    case 2:
+      *p = '*';
+      *q = '*';
+      break;
+    case 3:
+      *p = '/';
+      *q = '/';
+      break;
   }
 
   p ++;
+  q ++;
 }
 
 static uint8_t op_num;
@@ -63,6 +85,7 @@ static inline void gen_rand_expr() {
         gen_rand_expr();
       } else {
         gen_num();
+        gen(' ');
       }
 
       break;
@@ -76,6 +99,7 @@ static inline void gen_rand_expr() {
 
       if ((p - buf) > 24) {
         gen_num();
+        gen(' ');
       } else {
         gen_rand_expr();
         gen_rand_op();
@@ -97,14 +121,16 @@ int main(int argc, char *argv[]) {
   int i;
   for (i = 0; i < loop; i ++) {
     p = buf;
+    q = hex_buf;
     op_num = 0;
     
     memset(buf, '\0', sizeof(buf));
+    memset(hex_buf, '\0', sizeof(hex_buf));
     memset(code_buf, '\0', sizeof(code_buf));
     
     gen_rand_expr();
 
-    sprintf(code_buf, code_format, buf);
+    sprintf(code_buf, code_format, hex_buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
--- nemu/tools/gen-expr/hello.c
+++ nemu/tools/gen-expr/hello.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
 int main() {
-  unsigned result = 0xFFFFFF91/0x14/0x1E; 
-  unsigned re = -111/20/30;
-  printf("%u %u\n", result, re); 
-  return 0;
+  int result = 0xe-0x1;
 }
