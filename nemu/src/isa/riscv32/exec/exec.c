87bad808e629597b8456a339057113f04bd20f16
tracer-ics2020 <tracer@njuics.org>
2021-08-24 18:28:29 +0800

    >  compile 1190320065

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -27,7 +27,7 @@ static inline void fetch_decode_exec(DecodeExecState *s) {
     IDEX (0b00000, I, load)
     IDEX (0b01000, S, store)
     IDEX (0b01101, U, lui)
-    //IDEX (0b00100, U, li)
+    IDEX (0b00100, U, li)
     EX   (0b11010, nemu_trap)
     default: exec_inv(s);
   }
