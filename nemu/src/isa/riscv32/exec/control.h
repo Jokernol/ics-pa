ab4a7f2442f44d73256084d2f6764847062e2278
tracer-ics2020 <tracer@njuics.org>
2021-08-25 17:19:09 +0800

    >  compile 1190320065

--- nemu/src/isa/riscv32/exec/control.h
+++ nemu/src/isa/riscv32/exec/control.h
@@ -2,5 +2,8 @@ static inline def_EHelper(jal) {
   rtl_add(s, ddest, &s->seq_pc, rz);
   rtl_addi(s, &s->is_jmp, rz, 1);
   rtl_addi(s, &s->jmp_pc, &cpu.pc, s->src1.imm);
+
+  print_Dop(id_src1->str, OP_STR_SIZE, "0x%x", s->jmp_pc);
+
   print_asm_template2(jal);
 }
