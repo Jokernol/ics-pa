3edba77ce812de43ac5fc9919292138628d439eb
tracer-ics2020 <tracer@njuics.org>
2021-08-25 17:50:40 +0800

    >  compile 1190320065

--- nemu/src/isa/riscv32/local-include/decode.h
+++ nemu/src/isa/riscv32/local-include/decode.h
@@ -34,8 +34,7 @@ static inline def_DHelper(U) {
 }
 
 static inline def_DHelper(J) {
-  word_t simm = (s->isa.instr.j.simm20 << 20) + (s->isa.instr.j.imm19_12 << 12) + (s->isa.instr.j.imm11 << 11) + (s->isa.instr.j.imm10_1 << 1);
-  print_asm(str(hello) "%c %x", suffix_char(id_dest->width), simm);
+  sword_t simm = (s->isa.instr.j.simm20 << 20) + (s->isa.instr.j.imm19_12 << 12) + (s->isa.instr.j.imm11 << 11) + (s->isa.instr.j.imm10_1 << 1);
   decode_op_i(s, id_src1, simm, true);
   decode_op_r(s, id_dest, s->isa.instr.j.rd, false);
 }
