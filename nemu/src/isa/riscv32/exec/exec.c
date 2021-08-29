f4d1c374459f894a40c8023a5519237164e230cc
tracer-ics2020 <tracer@njuics.org>
2021-08-29 15:16:33 +0800

    >  compile 1190320065

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -8,8 +8,11 @@ static inline void set_width(DecodeExecState *s, int width) {
 
 static inline def_EHelper(load) {
   switch (s->isa.instr.i.funct3) {
-    EXW  (0b010, ld, 4)
-    EXW  (0b001, lds, 2)
+    EXW (0b010, ld, 4)
+    EXW (0b101, ld, 2)
+    EXW (0b100, ld, 1)
+    EXW (0b001, lds, 2)
+    EXW (0b000, lds, 1)
     default: exec_inv(s);
   }
 }
